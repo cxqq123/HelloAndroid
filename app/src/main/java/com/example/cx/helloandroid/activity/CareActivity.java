d1e0f28ece6559a86909265662a8ccc4a8c57333
cx <744185734@qq.com>
2017-09-28 14:32:48 +0800

    一些小功能--2017-9-28

--- app/src/main/java/com/example/cx/helloandroid/activity/CareActivity.java
+++ app/src/main/java/com/example/cx/helloandroid/activity/CareActivity.java
@@ -1,7 +1,5 @@
 package com.example.cx.helloandroid.activity;
 
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,43 +7,24 @@ import android.widget.Button;
 
 import com.example.cx.helloandroid.R;
 
-public class CareActivity extends AppCompatActivity {
-    private Button btnCare;
-    private boolean isFlag;
 
-    private boolean isSecondFlag;
+public class CareActivity extends AppCompatActivity {
+    private Button mTestBt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_care);
-        btnCare = (Button) findViewById(R.id.btn_care);
-        // 若Android 系统版本 高于5.0 则直接显示透明式状态栏
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            isSecondFlag =true;
-        }else{
-            //若是5.0以上的手机
-            isSecondFlag=false;
-        }
-        if(isSecondFlag){
-            btnCare.setBackgroundResource(R.drawable.bg_ripper);
-        }else{
-            btnCare.setBackgroundColor(Color.parseColor("#f60"));
-        }
-
-        btnCare.setOnClickListener(new View.OnClickListener() {
+        mTestBt = (Button) findViewById(R.id.test_bt);
+        mTestBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!isFlag){
-                    btnCare.setText("已关注");
-                    isFlag =true;
-                }else{
-                    btnCare.setText("关注");
-                    isFlag=false;
+                if (mTestBt.getText().toString().equals("关注")) {
+                    mTestBt.setText("未关注");
+                } else {
+                    mTestBt.setText("关注");
                 }
             }
         });
-
-
-
     }
 }
--- app/src/main/java/com/example/cx/helloandroid/utils/NetUtil.java
+++ app/src/main/java/com/example/cx/helloandroid/utils/NetUtil.java
@@ -1,6 +1,9 @@
 package com.example.cx.helloandroid.utils;
 
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
@@ -43,5 +46,32 @@ public class NetUtil {
             netType = 1;
         }
         return netType;
+    }
+
+    /**
+     * 判断是否是wifi连接
+     */
+    public static boolean isWifi(Context context)
+    {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (cm == null)
+            return false;
+        return cm.getActiveNetworkInfo().getType() == ConnectivityManager.TYPE_WIFI;
+
+    }
+
+    /**
+     * 打开网络设置界面
+     */
+    public static void openSetting(Activity activity)
+    {
+        Intent intent = new Intent("/");
+        ComponentName cm = new ComponentName("com.android.settings",
+                "com.android.settings.WirelessSettings");
+        intent.setComponent(cm);
+        intent.setAction("android.intent.action.VIEW");
+        activity.startActivityForResult(intent, 0);
     }
 }
